cecb11802961ba83cfbf5bcdad644a2a5da4089c
Spencer Hartland <spencer@spencerhartland.com>
2026-07-12 16:48:46 -0700

    Initial Commit

---
+++ Harness/HarnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarnessApp.swift
+//  Harness
+//
+//  Created by Spencer Hartland on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct HarnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
